ba41a87efffc3259400dad1d43d25abb50b96050
asbdu45122cs11 <144931115+asbdu45122cs11@users.noreply.github.com>
2023-09-13 09:32:10 -0700

    Add files via upload

---
+++ Challenge-unit 1/logical operators.py
@@ -0,0 +1,7 @@
+a,b,c,d=10,5,2,1
+
+print ((a>b) and (c>d))
+
+print((a>b) or (d>c))
+
+print (not (a>b))
---
+++ Challenge-unit 1/random number .py
@@ -0,0 +1,9 @@
+import random
+s='abcde'
+print("choice (abcde):", random. choice (s))
+list = [10,2,3,1,8,19]
+print("shuffle (list):", random. shuffle (list))
+print ("random.seed (20):", random.seed (20))
+print ("random():", random.random())
+print ("uniform (2,3):", random. uniform (2,3))
+print ("randrange (2, 10, 1):", random.randrange (2,10,1))
